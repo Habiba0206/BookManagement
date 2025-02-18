80d9d050873c491ea72d8afa9d1a423263ebe960
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-18 23:39:00 +0500

    Created notification entities

---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/EmailTemplate.cs
@@ -0,0 +1,13 @@
+﻿using Type = BookManagement.Domain.Enums.NotificationType;
+
+namespace BookManagement.Domain.Entities;
+
+public class EmailTemplate : NotificationTemplate
+{
+    public EmailTemplate()
+    {
+        Type = Type.Email;
+    }
+
+    public string Subject { get; set; } = default!;
+}
---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/NotificationTemplate.cs
@@ -0,0 +1,16 @@
+﻿using BookManagement.Domain.Common.Entities;
+using BookManagement.Domain.Enums;
+
+namespace BookManagement.Domain.Entities;
+
+public abstract class NotificationTemplate : IEntity
+{
+    public NotificationType Type { get; set; }
+
+    public NotificationTemplateType TemplateType { get; set; }
+
+    public string Content { get; set; } = default!;
+
+    public IList<NotificationHistory> Histories { get; set; } = new List<NotificationHistory>();
+    public Guid Id { get; set; }
+}
