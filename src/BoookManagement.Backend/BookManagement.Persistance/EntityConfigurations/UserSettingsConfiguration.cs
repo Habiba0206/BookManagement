f032402cd32b5e2064c50fb8d9814addcfeabddd
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 00:43:40 +0500

    Created user and user settings configutation

---
+++ src/BoookManagement.Backend/BookManagement.Persistance/EntityConfigurations/UserSettingsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Persistance.EntityConfigurations;
+
+public class UserSettingsConfiguration : IEntityTypeConfiguration<UserSettings>
+{
+    public void Configure(EntityTypeBuilder<UserSettings> builder)
+    {
+        builder.HasOne<User>().WithOne(user => user.UserSettings).HasForeignKey<UserSettings>(settings => settings.Id);
+    }
+}
