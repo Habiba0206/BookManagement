2a57d4883e8fab64b2ed8ca8bc504e247606c1c2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-18 23:38:39 +0500

    Created user and user settings entities

---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using BookManagement.Domain.Common.Entities;
+using BookManagement.Domain.Enums;
+
+namespace BookManagement.Domain.Entities;
+
+public class User : AuditableEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string PasswordHash { get; set; }
+    public int Age { get; set; }
+    public bool IsEmailAddressVerified { get; set; }
+    public Role Role { get; set; }
+    public UserState UserState { get; set; }
+    public UserSettings? UserSettings { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿using BookManagement.Domain.Common.Entities;
+using BookManagement.Domain.Enums;
+
+namespace BookManagement.Domain.Entities;
+
+public class UserSettings : IEntity
+{
+    public NotificationType? PreferredNotificationType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the user Id
+    /// </summary>
+    public Guid Id { get; set; }
+}
