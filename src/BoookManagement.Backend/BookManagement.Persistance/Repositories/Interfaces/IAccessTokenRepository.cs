ca6163116833eb83d544b6ebb726d27497bd9bbc
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 00:42:43 +0500

    Created access token repository

---
+++ src/BoookManagement.Backend/BookManagement.Persistance/Repositories/Interfaces/IAccessTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookManagement.Domain.Common.Commands;
+using BookManagement.Domain.Entities;
+
+namespace BookManagement.Persistance.Repositories.Interfaces;
+
+public interface IAccessTokenRepository
+{
+    ValueTask<AccessToken> CreateAsync(
+        AccessToken accessToken,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken?> GetByIdAsync(
+        Guid accessTokeId,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken> UpdateAsync(
+        AccessToken accessToken,
+        CancellationToken cancellationToken = default);
+}
