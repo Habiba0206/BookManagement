3759ec22c0be0f4b0196238941f15d8aa06d9ea4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 00:44:13 +0500

    Created book configuration

---
+++ src/BoookManagement.Backend/BookManagement.Persistance/EntityConfigurations/BookConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BookManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Persistence.EntityConfigurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder
+            .HasIndex(b => b.Title)
+            .IsUnique();
+
+        builder
+            .Property(b => b.Title)
+            .IsRequired();
+
+        builder.HasOne(b => b.User)
+            .WithMany(u => u.Books)
+            .HasForeignKey(b => b.UserId);
+    }
+}
