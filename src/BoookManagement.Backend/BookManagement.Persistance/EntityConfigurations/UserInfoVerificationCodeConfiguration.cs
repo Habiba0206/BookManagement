bfd58b212616deb0d96f3c60ab50b2f4d04fc174
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 00:43:24 +0500

    Created verification configurations

---
+++ src/BoookManagement.Backend/BookManagement.Persistance/EntityConfigurations/UserInfoVerificationCodeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookManagement.Persistance.EntityConfigurations;
+
+public class UserInfoVerificationCodeConfiguration : IEntityTypeConfiguration<UserInfoVerificationCode>
+{
+    public void Configure(EntityTypeBuilder<UserInfoVerificationCode> builder)
+    {
+        builder.HasOne<User>().WithMany().HasForeignKey(code => code.UserId);
+    }
+}
