00afed812636b9d455f4a63f2a49ccdb0222453e
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 00:44:36 +0500

    Created app db context

---
+++ src/BoookManagement.Backend/BookManagement.Persistance/DataContexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BookManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Persistance.DataContexts;
+public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<UserSettings> UserSettings => Set<UserSettings>();
+    public DbSet<UserInfoVerificationCode> UserInfoVerificationCodes => Set<UserInfoVerificationCode>();
+    public DbSet<AccessToken> AccessTokens => Set<AccessToken>();
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<BookView> BookViews => Set<BookView>();
+    public DbSet<EmailTemplate> EmailTemplates => Set<EmailTemplate>();
+    public DbSet<EmailHistory> EmailHistories => Set<EmailHistory>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
