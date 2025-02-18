c6604c6ed0076b20cf9b049af6ab212f135ce080
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-18 23:38:19 +0500

    Created verification code entities

---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/UserInfoVerificationCode.cs
@@ -0,0 +1,13 @@
+﻿using BookManagement.Domain.Enums;
+
+namespace BookManagement.Domain.Entities;
+
+public class UserInfoVerificationCode : VerificationCode
+{
+    public UserInfoVerificationCode()
+    {
+        Type = VerificationType.UserInfoVerificationCode;
+    }
+
+    public Guid UserId { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/VerificationCode.cs
@@ -0,0 +1,19 @@
+﻿using BookManagement.Domain.Common.Entities;
+using BookManagement.Domain.Enums;
+
+namespace BookManagement.Domain.Entities;
+
+public abstract class VerificationCode : Entity
+{
+    public VerificationCodeType CodeType { get; set; }
+
+    public VerificationType Type { get; set; }
+
+    public DateTimeOffset ExpiryTime { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string Code { get; set; } = default!;
+
+    public string VerificationLink { get; set; } = default!;
+}
