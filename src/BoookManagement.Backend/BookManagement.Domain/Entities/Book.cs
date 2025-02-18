d88d03e2b8470353132fe0eaa0631634446129ae
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-18 23:39:09 +0500

    Created book entities

---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿using BookManagement.Domain.Common.Entities;
+
+namespace BookManagement.Domain.Entities;
+
+public class Book : AuditableEntity
+{
+    public Guid UserId { get; set; }
+    public string Title { get; set; } = default!;
+    public string Author { get; set; } = default;
+    public int PublicationYear { get; set; }
+    public int ViewsCount { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/BookView.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Domain.Common.Entities;
+
+namespace BookManagement.Domain.Entities;
+
+public class BookView : AuditableEntity
+{
+    public Guid BookId { get; set; }
+    public DateTimeOffset AccessedAt { get; set; } 
+}
