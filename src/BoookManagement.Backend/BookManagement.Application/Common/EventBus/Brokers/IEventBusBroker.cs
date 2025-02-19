2861d130338551cb55fcac6f75fdb62f5920f6fd
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:56:48 +0500

    Created event bus broker

---
+++ src/BoookManagement.Backend/BookManagement.Application/Common/EventBus/Brokers/IEventBusBroker.cs
@@ -0,0 +1,16 @@
+﻿using BookManagement.Domain.Common.Events;
+
+namespace BookManagement.Application.Common.EventBus.Brokers;
+
+public interface IEventBusBroker
+{
+    ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+
+    ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+}
