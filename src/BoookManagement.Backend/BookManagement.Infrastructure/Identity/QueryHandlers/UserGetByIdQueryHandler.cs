5ede27d4b3a0c0b4c081167478cdbe5823fd67d0
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:04:09 +0500

    Created user query handlers

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/QueryHandlers/UserGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookManagement.Application.Identity.Models;
+using BookManagement.Application.Identity.Queries;
+using BookManagement.Application.Identity.Services;
+using BookManagement.Domain.Common.Queries;
+
+namespace BookManagement.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetByIdQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetByIdQuery, UserDto>
+{
+    public async Task<UserDto> Handle(UserGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.GetByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(result);
+    }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/QueryHandlers/UserGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BookManagement.Application.Identity.Models;
+using BookManagement.Application.Identity.Queries;
+using BookManagement.Application.Identity.Services;
+using BookManagement.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetQuery, ICollection<UserDto>>
+{
+    public async Task<ICollection<UserDto>> Handle(UserGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.Get(
+            request.UserFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<UserDto>>(result);
+    }
+}
