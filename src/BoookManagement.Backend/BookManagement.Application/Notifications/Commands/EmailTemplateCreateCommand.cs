7c00d9fb75f5e7ef8f3e5a078d9fb43647b14935
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:00:27 +0500

    Created email command

---
+++ src/BoookManagement.Backend/BookManagement.Application/Notifications/Commands/EmailTemplateCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Application.Notifications.Models;
+using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Application.Notifications.Commands;
+
+public class EmailTemplateCreateCommand : ICommand<EmailTemplateDto>
+{
+    public EmailTemplateDto EmailTemplateDto { get; set; }
+}
