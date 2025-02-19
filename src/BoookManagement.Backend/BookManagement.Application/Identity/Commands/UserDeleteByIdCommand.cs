084e781c739d14197c7a98c48116cdcfb437da0b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:59:07 +0500

    Created user commands

---
+++ src/BoookManagement.Backend/BookManagement.Application/Identity/Commands/UserDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Application.Identity.Commands;
+
+public class UserDeleteByIdCommand : ICommand<bool>
+{
+    public Guid UserId { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Application/Identity/Commands/UserUpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Application.Identity.Models;
+using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Application.Identity.Commands;
+
+public class UserUpdateCommand : ICommand<UserDto>
+{
+    public UserDto UserDto { get; set; }
+}
