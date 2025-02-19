98060d225faf17354b616b95a85dcb148d2f4856
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:56:31 +0500

    Created book queries

---
+++ src/BoookManagement.Backend/BookManagement.Application/Books/Queries/BookGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Application.Books.Models;
+using BookManagement.Domain.Common.Queries;
+
+namespace BookManagement.Application.Books.Queries;
+
+public class BookGetByIdQuery : IQuery<GetBookDto?>
+{
+    public Guid BookId { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Application/Books/Queries/BookGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Application.Books.Models;
+using BookManagement.Domain.Common.Queries;
+
+namespace BookManagement.Application.Books.Queries;
+
+public class BookGetQuery : IQuery<ICollection<GetBookDto>>
+{
+    public BookFilter BookFilter { get; set; }
+}
