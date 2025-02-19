aab027f9803f569edde91a66be34d35635986521
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:03:49 +0500

    Created user mapper

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookManagement.Application.Identity.Models;
+using BookManagement.Domain.Entities;
+
+namespace BookManagement.Infrastructure.Identity.Mappers;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<SignInDetails, User>().ReverseMap();
+        CreateMap<SignUpDetails, User>().ReverseMap();
+    }
+}
