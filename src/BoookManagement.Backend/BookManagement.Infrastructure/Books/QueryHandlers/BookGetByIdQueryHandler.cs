b8a41cf9c4900b548659f58bf60c5aed0706c5d3
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:02:32 +0500

    Created query handlers

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Books/QueryHandlers/BookGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookManagement.Application.Books.Models;
+using BookManagement.Application.Books.Queries;
+using BookManagement.Application.Books.Services;
+using BookManagement.Domain.Common.Queries;
+
+namespace BookManagement.Infrastructure.Books.QueryHandlers;
+
+public class BookGetByIdQueryHandler(
+    IMapper mapper,
+    IBookService bookService)
+    : IQueryHandler<BookGetByIdQuery, GetBookDto>
+{
+    public async Task<GetBookDto> Handle(BookGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await bookService.GetByIdAsync(request.BookId, cancellationToken: cancellationToken);
+
+        return mapper.Map<GetBookDto>(result);
+    }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Books/QueryHandlers/BookGetQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BookManagement.Application.Books.Models;
+using BookManagement.Application.Books.Queries;
+using BookManagement.Application.Books.Services;
+using BookManagement.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagement.Infrastructure.Books.QueryHandlers;
+
+public class BookGetQueryHandler(
+    IMapper mapper,
+    IBookService answerService)
+    : IQueryHandler<BookGetQuery, ICollection<GetBookDto>>
+{
+    public async Task<ICollection<GetBookDto>> Handle(BookGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await answerService.Get(
+            request.BookFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .OrderByDescending(book => book.PopularityScore)
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<GetBookDto>>(result);
+    }
+}
