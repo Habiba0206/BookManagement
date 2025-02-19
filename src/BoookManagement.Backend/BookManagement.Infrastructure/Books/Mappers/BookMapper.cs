b519469a23596552da09633f044283f8f0a48c62
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:02:23 +0500

    Created book mapper

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Books/Mappers/BookMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookManagement.Application.Books.Models;
+using BookManagement.Domain.Entities;
+
+namespace BookManagement.Infrastructure.Books.Mappers;
+
+public class BookMapper : Profile
+{
+    public BookMapper()
+    {
+        CreateMap<Book, GetBookDto>().ReverseMap();
+        CreateMap<Book, CreateBookDto>().ReverseMap();
+        CreateMap<GetBookDto, CreateBookDto>().ReverseMap();
+    }
+}
