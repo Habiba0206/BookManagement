2ec9c32f5cb1880135e6dd18d1a5e10bcbbafef5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:00:42 +0500

    Created notification events

---
+++ src/BoookManagement.Backend/BookManagement.Application/Notifications/Events/NotificationEvent.cs
@@ -0,0 +1,10 @@
+﻿using BookManagement.Domain.Common.Events;
+
+namespace BookManagement.Application.Notifications.Events;
+
+public record NotificationEvent : EventBase
+{
+    public Guid SenderUserId { get; set; }
+
+    public Guid ReceiverUserId { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Application/Notifications/Events/SendNotificationEvent.cs
@@ -0,0 +1,8 @@
+﻿using BookManagement.Application.Notifications.Models;
+
+namespace BookManagement.Application.Notifications.Events;
+
+public record SendNotificationEvent : NotificationEvent
+{
+    public NotificationMessage Message { get; set; } = default!;
+}
