960d41c12d1b978e6af93a84a26a176eafd3e73a
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:49:18 +0500

    Created verification validator

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Verification/Validators/UserInfoVerificationCodeValidator.cs
@@ -0,0 +1,32 @@
+﻿using BookManagement.Application.Common.Settings;
+using BookManagement.Domain.Entities;
+using BookManagement.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace BookManagement.Infrastructure.Verification.Validators;
+
+public class UserInfoVerificationCodeValidator : AbstractValidator<UserInfoVerificationCode>
+{
+    public UserInfoVerificationCodeValidator(IOptions<VerificationSettings> verificationSettings, IOptions<ValidationSettings> validationSettings)
+    {
+        var verificationSettingsValue = verificationSettings.Value;
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(code => code.UserId).NotEqual(Guid.Empty);
+
+                RuleFor(code => code.ExpiryTime)
+                    .GreaterThanOrEqualTo(DateTimeOffset.UtcNow)
+                    .LessThanOrEqualTo(DateTimeOffset.UtcNow.AddSeconds(verificationSettingsValue.VerificationCodeExpiryTimeInSeconds));
+
+                RuleFor(code => code.IsActive).Equal(true);
+
+                RuleFor(code => code.VerificationLink).NotEmpty().Matches(validationSettingsValue.UrlRegexPattern);
+            }
+        );
+    }
+}
