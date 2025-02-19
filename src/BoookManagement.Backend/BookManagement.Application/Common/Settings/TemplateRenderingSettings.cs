5840fd88f0ff9c26858498efa99d3c98164285b4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:58:36 +0500

    Created notification settings

---
+++ src/BoookManagement.Backend/BookManagement.Application/Common/Settings/TemplateRenderingSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManagement.Application.Common.Settings;
+
+public class TemplateRenderingSettings
+{
+    public string PlaceholderRegexPattern { get; set; } = default!;
+
+    public string PlaceholderValueRegexPattern { get; set; } = default!;
+
+    public int RegexMatchTimeoutInSeconds { get; set; }
+}
