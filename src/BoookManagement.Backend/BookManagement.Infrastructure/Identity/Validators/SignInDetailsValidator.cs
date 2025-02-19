0168898451306eb1d485749d1cec1b9d254a8ac8
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:04:41 +0500

    Created identity validators

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/Validators/SignInDetailsValidator.cs
@@ -0,0 +1,18 @@
+﻿using BookManagement.Application.Common.Settings;
+using BookManagement.Application.Identity.Models;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace BookManagement.Infrastructure.Identity.Validators;
+
+public class SignInDetailsValidator : AbstractValidator<SignInDetails>
+{
+    public SignInDetailsValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleFor(x => x.EmailAddress).NotEmpty().Matches(validationSettingsValue.EmailAddressRegexPattern);
+
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
