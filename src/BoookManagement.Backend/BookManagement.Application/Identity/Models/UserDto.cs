d4431d44ae1739328e694831e1773b9d2f26b676
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:59:30 +0500

    Created user dto and filter models

---
+++ src/BoookManagement.Backend/BookManagement.Application/Identity/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using BookManagement.Domain.Enums;
+
+namespace BookManagement.Application.Identity.Models;
+
+public class UserDto
+{
+    public Guid? Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public int Age { get; set; }
+    public Role Role { get; set; }
+    public UserState UserState { get; set; }
+}
