b457c563964025741a567745bb4046eac5f58512
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:51:01 +0500

    Created book commands

---
+++ src/BoookManagement.Backend/BookManagement.Application/Books/Commands/BookDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Application.Books.Commands;
+
+public class BookDeleteByIdCommand : ICommand<bool>
+{
+    public Guid BookId { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Application/Books/Commands/BookUpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using BookManagement.Application.Books.Models;
+using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Application.Books.Commands;
+
+public class BookUpdateCommand : ICommand<CreateBookDto>
+{
+    public CreateBookDto BookDto { get; set; }
+}
