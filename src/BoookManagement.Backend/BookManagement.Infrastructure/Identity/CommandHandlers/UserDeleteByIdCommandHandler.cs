d5531befa340a60f3f0492debc6c4db610c57a23
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:03:41 +0500

    Created user command handlers

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/CommandHandlers/UserDeleteByIdCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using BookManagement.Application.Identity.Commands;
+using BookManagement.Application.Identity.Services;
+using BookManagement.Domain.Common.Commands;
+
+namespace BookManagement.Infrastructure.Identity.CommandHandlers;
+
+public class UserDeleteByIdCommandHandler(
+    IUserService userService)
+    : ICommandHandler<UserDeleteByIdCommand, bool>
+{
+    public async Task<bool> Handle(UserDeleteByIdCommand request, CancellationToken cancellationToken)
+    {
+        var result = await userService.DeleteByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return result is not null;
+    }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Identity/CommandHandlers/UserUpdateCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BookManagement.Application.Identity.Commands;
+using BookManagement.Application.Identity.Models;
+using BookManagement.Application.Identity.Services;
+using BookManagement.Domain.Common.Commands;
+using BookManagement.Domain.Entities;
+
+namespace BookManagement.Infrastructure.Identity.CommandHandlers;
+
+public class UserUpdateCommandHandler(
+    IMapper mapper,
+    IUserService userService) : ICommandHandler<UserUpdateCommand, UserDto>
+{
+    public async Task<UserDto> Handle(UserUpdateCommand request, CancellationToken cancellationToken)
+    {
+        var user = mapper.Map<User>(request.UserDto);
+
+        var updatedUser = await userService.UpdateAsync(user, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(updatedUser);
+    }
+}
