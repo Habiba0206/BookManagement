0bf6c7dfba65a57f6f1cee8ad0b21eaae8ce319c
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:02:55 +0500

    Created book validator

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Books/Validators/BookValidator.cs
@@ -0,0 +1,13 @@
+﻿using BookManagement.Domain.Entities;
+using BookManagement.Domain.Enums;
+using FluentValidation;
+
+namespace BookManagement.Infrastructure.Books.Validators;
+public class BookValidator : AbstractValidator<Book>
+{
+    public BookValidator()
+    {
+        RuleFor(book => book.Title).NotEmpty().MinimumLength(2);
+        RuleFor(book => book.Author).NotEmpty().MinimumLength(2);
+    }
+}
