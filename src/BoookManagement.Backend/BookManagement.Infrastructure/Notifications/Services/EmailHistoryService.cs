f4fdd5c737165f02f9851403e1f2e6b66c81f2c5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:06:10 +0500

    Implemented notification services

---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Notifications/Services/EmailHistoryService.cs
@@ -0,0 +1,63 @@
+﻿using BookManagement.Infrastructure.Notifications.Validators;
+using BookManagement.Application.Notifications.Models;
+using BookManagement.Application.Notifications.Services;
+using BookManagement.Domain.Common.Commands;
+using BookManagement.Domain.Common.Queries;
+using BookManagement.Domain.Entities;
+using BookManagement.Domain.Enums;
+using BookManagement.Persistence.Extensions;
+using BookManagement.Persistence.Repositories.Interfaces;
+using FluentValidation;
+using System.Linq.Expressions;
+
+namespace BookManagement.Infrastructure.Notifications.Services;
+
+public class EmailHistoryService(
+    IEmailHistoryRepository emailHistoryRepository,
+    EmailHistoryValidator emailHistoryValidator)
+    : IEmailHistoryService
+{
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    emailHistoryRepository.CheckByIdAsync(id, cancellationToken);
+
+    public IQueryable<EmailHistory> Get(
+        Expression<Func<EmailHistory, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    emailHistoryRepository.Get(predicate, queryOptions);
+
+    public IQueryable<EmailHistory> Get(
+        EmailHistoryFilter emailHistoryFilter,
+        QueryOptions queryOptions = default) =>
+    emailHistoryRepository.
+        Get(queryOptions: queryOptions)
+        .ApplyPagination(emailHistoryFilter);
+
+    public ValueTask<EmailHistory?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    emailHistoryRepository.GetByIdAsync(id, queryOptions);
+
+    public ValueTask<IList<EmailHistory>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    emailHistoryRepository.GetByIdsAsync(ids, queryOptions);
+
+    public async ValueTask<EmailHistory> CreateAsync(
+        EmailHistory emailHistory,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await emailHistoryValidator.ValidateAsync(
+            emailHistory,
+            options => options.IncludeRuleSets(EntityEvent.OnCreate.ToString()),
+            cancellationToken
+        );
+        if (!validationResult.IsValid) throw new ValidationException(validationResult.Errors);
+
+        return await emailHistoryRepository.CreateAsync(emailHistory, commandOptions, cancellationToken);
+    }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Infrastructure/Notifications/Services/EmailTemplateService.cs
@@ -0,0 +1,66 @@
+﻿using BookManagement.Infrastructure.Notifications.Validators;
+using BookManagement.Application.Notifications.Models;
+using BookManagement.Application.Notifications.Services;
+using BookManagement.Domain.Common.Commands;
+using BookManagement.Domain.Common.Queries;
+using BookManagement.Domain.Entities;
+using BookManagement.Domain.Enums;
+using BookManagement.Persistence.Extensions;
+using BookManagement.Persistence.Repositories.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BookManagement.Infrastructure.Notifications.Services;
+
+public class EmailTemplateService(
+    IEmailTemplateRepository emailTemplateRepository,
+    EmailTemplateValidator emailTemplateValidator) :
+    IEmailTemplateService
+{
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    emailTemplateRepository.CheckByIdAsync(id, cancellationToken);
+
+    public IQueryable<EmailTemplate> Get(
+        Expression<Func<EmailTemplate, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    emailTemplateRepository.Get(predicate, queryOptions);
+
+    public IQueryable<EmailTemplate> Get(
+        EmailTemplateFilter emailTemplateFilter,
+        QueryOptions queryOptions = default) =>
+    emailTemplateRepository
+        .Get(queryOptions: queryOptions)
+        .ApplyPagination(emailTemplateFilter);
+
+    public ValueTask<EmailTemplate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    emailTemplateRepository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<EmailTemplate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    emailTemplateRepository.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public async ValueTask<EmailTemplate?> GetByTypeAsync(
+        NotificationTemplateType templateType,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    await emailTemplateRepository
+        .Get(template => template.TemplateType == templateType, queryOptions)
+            .SingleOrDefaultAsync(cancellationToken);
+
+    public ValueTask<EmailTemplate> CreateAsync(EmailTemplate emailTemplate, CommandOptions commandOptions = default, CancellationToken cancellationToken = default)
+    {
+        var validationResult = emailTemplateValidator.Validate(emailTemplate);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        return emailTemplateRepository.CreateAsync(emailTemplate, commandOptions, cancellationToken);
+    }
+}
