db49087d3131488fe73b619148199e3a09bb4036
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:01:52 +0500

    Updated entity

--- src/BoookManagement.Backend/BookManagement.Domain/Entities/Book.cs
+++ src/BoookManagement.Backend/BookManagement.Domain/Entities/Book.cs
@@ -7,7 +7,9 @@ public class Book : AuditableEntity
     public Guid UserId { get; set; }
     public string Title { get; set; } = default!;
     public string Author { get; set; } = default!;
+    public string Description { get; set; }
     public int PublicationYear { get; set; }
     public int ViewsCount { get; set; }
+    public double PopularityScore { get; set; }
     public User User { get; set; }
 }
