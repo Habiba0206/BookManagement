b0f0c0375029a8e953032f7bfa3df3dbecb67b7a
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-20 00:00:14 +0500

    Created notification broker

---
+++ src/BoookManagement.Backend/BookManagement.Application/Notifications/Brokers/IEmailSenderBroker.cs
@@ -0,0 +1,8 @@
+﻿using BookManagement.Application.Notifications.Models;
+
+namespace BookManagement.Application.Notifications.Brokers;
+
+public interface IEmailSenderBroker
+{
+    ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken = default);
+}
