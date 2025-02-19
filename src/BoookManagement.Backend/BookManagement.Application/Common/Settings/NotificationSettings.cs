a76eea7a1b75b5c5fc6bd157630e18a5190fd84d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-02-19 23:57:33 +0500

    Created notification settings

---
+++ src/BoookManagement.Backend/BookManagement.Application/Common/Settings/NotificationSettings.cs
@@ -0,0 +1,8 @@
+using BookManagement.Domain.Enums;
+
+namespace BookManagement.Application.Common.Settings;
+
+public class NotificationSettings
+{
+    public NotificationType DefaultNotificationType { get; set; }
+}
---
+++ src/BoookManagement.Backend/BookManagement.Application/Common/Settings/NotificationSubscriberSettings.cs
@@ -0,0 +1,5 @@
+﻿namespace BookManagement.Application.Common.Settings;
+
+public class NotificationSubscriberSettings : EventBusSubscriberSettings
+{
+}
